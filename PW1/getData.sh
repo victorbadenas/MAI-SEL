263e2fb7124a169547d7cd601d769f16b8c9d4c7
Victor Badenas <victor.badenas@gmail.com>
2021-03-17 14:43:25 +0100

    added getData.sh

---
+++ PW1/getData.sh
@@ -0,0 +1,9 @@
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data -O data/tic-tac-toe.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.names -O data/tic-tac-toe.names
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.standardized.data -O data/audiology.standardized.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.standardized.names -O data/audiology.standardized.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.standardized.test -O data/audiology.standardized.test
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/plants/plants.data -O data/plants.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/plants/plants.data -O data/plants.names
