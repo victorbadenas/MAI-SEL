db417b35ac4159f530e35bbe27e2547b67fa6786
Victor Badenas <victor.badenas@gmail.com>
2021-03-19 14:33:57 +0100

    removed convert to numerical

--- PW1/src/dataset.py
+++ PW1/src/dataset.py
@@ -12,7 +12,6 @@ class Dataset:
         self.mode = mode
         self.checkArgs()
         self.data = self.loadDataset(datasetPath)
-        self.convertToNumerical()
         self.data.columns = self.data.columns.to_list()[:-1] + ['target']
 
     def checkArgs(self):
@@ -21,31 +20,6 @@ class Dataset:
 
     def loadDataset(self, datasetPath):
         return pd.read_csv(datasetPath, header=None)
-
-    def convertToNumerical(self):
-        if self.mode == GLOBAL:
-            self.convertGlobal()
-        elif self.mode == COLUMNS:
-            self.convertPerColumn()
-        self.convertLabels()
-
-    def convertGlobal(self):
-        self.labelEncoder = LabelEncoder()
-        flattenedData = self.data[self.data.columns[:-1]].to_numpy().flatten()
-        self.labelEncoder.fit(flattenedData)
-        for columnName in self.data.columns[:-1]:
-            self.data[columnName] = self.labelEncoder.transform(self.data[columnName])
-
-    def convertLabels(self):
-        self.targetEncoder = LabelEncoder()
-        targetName = self.data.columns[-1]
-        self.data[targetName] = self.targetEncoder.fit_transform(self.data[targetName])
-
-    def convertPerColumn(self):
-        self.encoders = dict()
-        for columnName in self.data.columns[:-1]:
-            self.encoders[columnName] = LabelEncoder()
-            self.data[columnName] = self.encoders[columnName].fit_transform(self.data[columnName])
 
     def __str__(self):
         return str(self.data)
