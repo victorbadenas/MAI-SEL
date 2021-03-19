d2531f2c863fcd356d1d008ce4e7c74ad620c781
Victor Badenas <victor.badenas@gmail.com>
2021-03-19 22:38:00 +0100

    added saver

--- PW1/src/main.py
+++ PW1/src/main.py
@@ -25,6 +25,7 @@ def parseArgumentsFromCommandLine():
     parser.add_argument('-l', "--logger", type=Path, default="log/log.log")
     parser.add_argument('-i', "--dataset_path", type=Path, required=True)
     parser.add_argument('-d', "--debug", action="store_true", default=False)
+    parser.add_argument('-f', "--output_dir", type=Path, default=None)
     return parser.parse_args()
 
 
@@ -32,6 +33,7 @@ class Main:
     def __init__(self, args):
         self.args = args
         self.show_parameters(args)
+        self.output_dir = self.args.output_dir
         self.dataset = Dataset(self.args.dataset_path)
         logging.debug(self.dataset)
 
@@ -42,12 +44,18 @@ class Main:
             logging.info(f"\t{label}: {value}")
 
     def __call__(self):
-        Y = self.dataset['target']
-        X = self.dataset.drop('target', axis=1)
+        X = self.dataset.inputData
+        Y = self.dataset.targetData
         prism = Prism()
         prism.fit(X, Y)
         logging.debug('\n'.join(map(str, prism.rules)))
-
+        if self.output_dir is not None:
+            json_model_path = self.output_dir / (self.dataset.name + '.json')
+            txt_model_path = self.output_dir / (self.dataset.name + '.rules')
+            logging.info(f"Saving json model to {json_model_path}")
+            prism.save(json_model_path)
+            logging.info(f"Saving json model to {txt_model_path}")
+            prism.save(txt_model_path)
 
 if __name__ == "__main__":
     args = parseArgumentsFromCommandLine()
