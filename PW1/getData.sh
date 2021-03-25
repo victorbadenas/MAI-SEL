fb5a5454f0f7bb19bcb99eeaecd9d094ddbee88d
Victor Badenas <victor.badenas@gmail.com>
2021-03-25 16:42:05 +0100

    added car dataset

--- PW1/getData.sh
+++ PW1/getData.sh
@@ -10,7 +10,7 @@ get_data () {
 
 mkdir data/
 
-get_data https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data '0,1,2,3,4,5,6,7,8,target'
+# get_data https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data '0,1,2,3,4,5,6,7,8,target'
 
 get_data https://archive.ics.uci.edu/ml/machine-learning-databases/hayes-roth/hayes-roth.data 'name,hobby,age,educational level,marital status,class'
 python -c "import pandas as pd; data=pd.read_csv('data/hayes-roth.data'); data=data.drop('name', axis=1); data.to_csv('data/hayes-roth.train', index=False)"
@@ -19,3 +19,5 @@ rm data/hayes-roth.data
 get_data https://archive.ics.uci.edu/ml/machine-learning-databases/hayes-roth/hayes-roth.test 'hobby,age,educational level,marital status,class'
 
 get_data https://archive.ics.uci.edu/ml/machine-learning-databases/chess/king-rook-vs-king-pawn/kr-vs-kp.data 'bkblk,bknwy,bkon8,bkona,bkspr,bkxbq,bkxcr,bkxwp,blxwp,bxqsq,cntxt,dsopp,dwipd,hdchk,katri,mulch,qxmsq,r2ar8,reskd,reskr,rimmx,rkxwp,rxmsq,simpl,skach,skewr,skrxp,spcop,stlmt,thrsk,wkcti,wkna8,wknck,wkovl,wkpos,wtoeg,class'
+
+get_data https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data 'buying,maint,doors,persons,lug_boot,safety,class'
