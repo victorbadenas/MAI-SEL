7bf37243f5468315a42792847afc8e8450899f80
Victor <victor.badenas@gmail.com>
2021-03-25 11:31:38 +0100

    basic train bash script

---
+++ PW1/train.sh
@@ -0,0 +1,3 @@
+python src/train.py -i data/hayes-roth.train -f models/ -l log/hayes-roth.train.log
+python src/train.py -i data/kr-vs-kp.data -f models/ -l log/kr-vs-kp.train.log
+python src/train.py -i data/tic-tac-toe.data -f models/ -l log/tic-tac-toe.train.log
