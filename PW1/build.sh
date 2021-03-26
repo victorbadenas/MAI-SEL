60495469f4f7c900967622d888ed4babf5824078
Victor Badenas <victor.badenas@gmail.com>
2021-03-26 12:10:02 +0100

    *.spec

--- PW1/build.sh
+++ PW1/build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-rm -r build/ dist/ train.spec test.spec
+rm -r build/ dist/ *.spec
 
 cd src/
 pyinstaller --hidden-import cmath train.py
 pyinstaller --hidden-import cmath test.py
 mv dist/ ../dist/
 mv build/ ../build/
-mv train.spec ../train.spec
-mv test.spec ../test.spec
+mv *.spec ../*.spec
 cd -
