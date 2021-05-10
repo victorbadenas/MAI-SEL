ae15750e994e21ec91079b430459e448e8359120
Victor Badenas <victor.badenas@gmail.com>
2021-05-10 19:29:54 +0200

    fixed multithreading random seed bug

--- PW2/src/decision_forest.py
+++ PW2/src/decision_forest.py
@@ -1,10 +1,13 @@
 from base_forest import BaseForestClassifier
 from tree_units import Tree
 import random
+import numpy as np
 
 class DecisionForestClassifier(BaseForestClassifier):
     def _fit_tree(self, tree_idx, X):
+        self.__restart_seed(tree_idx)
         X = self.__select_features_from_dataset(tree_idx, X)
+        print(X.columns)
         attributes = list(X.columns)[:-1]
         tree = Tree(attributes, self._labels, classKey=self.classKey, F=-1)
         return tree.fit(X)
@@ -13,11 +16,15 @@ class DecisionForestClassifier(BaseForestClassifier):
         X, Y = X.drop(self.classKey, axis=1), X[self.classKey]
         n_features = len(X.columns)
         if self.F == 'random':
-            F = random.randint(1, n_features)
+            F = random.randint(1, n_features+1)
         else:
             F = self.F
         F = min(F, n_features)
         return X.sample(n=F, axis='columns').join(Y)
+
+    def __restart_seed(self, seed):
+        random.seed(seed)
+        np.random.seed(seed)
 
     def _save_to_json(self, path_to_file):
         super()._save_to_json(path_to_file)
--- PW2/src/random_forest.py
+++ PW2/src/random_forest.py
@@ -1,8 +1,11 @@
 from base_forest import BaseForestClassifier
 from tree_units import Tree
+import random
+import numpy as np
 
 class RandomForestClassifier(BaseForestClassifier):
     def _fit_tree(self, tree_idx, X):
+        self.__restart_seed(tree_idx)
         X = self.__get_bootstrapped_dataset(X)
         tree = Tree(self._attributes, self._labels, classKey=self.classKey, F=self.F)
         return tree.fit(X)
@@ -10,3 +13,7 @@ class RandomForestClassifier(BaseForestClassifier):
     @staticmethod
     def __get_bootstrapped_dataset(X):
         return X.sample(n=len(X), axis='rows', replace=True)
+
+    def __restart_seed(self, seed):
+        random.seed(seed)
+        np.random.seed(seed)
